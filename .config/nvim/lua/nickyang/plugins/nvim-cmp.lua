cc720bc1e21e5013f69801442d45b0e64e84c8bf
Nick <nickdevhouse@gmail.com>
2023-04-16 11:16:46 +1200

    Show colors in cmp for tailwindcss
    Show colors in cmp for tailwindcss

--- .config/nvim/lua/nickyang/plugins/nvim-cmp.lua
+++ .config/nvim/lua/nickyang/plugins/nvim-cmp.lua
@@ -1,19 +1,19 @@
 -- import nvim-cmp plugin safely
 local cmp_status, cmp = pcall(require, "cmp")
 if not cmp_status then
-  return
+	return
 end
 
 -- import luasnip plugin safely
 local luasnip_status, luasnip = pcall(require, "luasnip")
 if not luasnip_status then
-  return
+	return
 end
 
 -- import lspkind plugin safely
 local lspkind_status, lspkind = pcall(require, "lspkind")
 if not lspkind_status then
-  return
+	return
 end
 
 -- load vs-code like snippets from plugins (e.g. friendly-snippets)
@@ -22,32 +22,61 @@ require("luasnip/loaders/from_vscode").lazy_load()
 vim.opt.completeopt = "menu,menuone,noselect"
 
 cmp.setup({
-  snippet = {
-    expand = function(args)
-      luasnip.lsp_expand(args.body)
-    end,
-  },
-  mapping = cmp.mapping.preset.insert({
-    ["<C-k>"] = cmp.mapping.select_prev_item(), -- previous suggestion
-    ["<C-j>"] = cmp.mapping.select_next_item(), -- next suggestion
-    ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-    ["<C-f>"] = cmp.mapping.scroll_docs(4),
-    ["<C-Space>"] = cmp.mapping.complete(), -- show completion suggestions
-    ["<C-e>"] = cmp.mapping.abort(), -- close completion window
-    ["<CR>"] = cmp.mapping.confirm({ select = false }),
-  }),
-  -- sources for autocompletion
-  sources = cmp.config.sources({
-    { name = "nvim_lsp" }, -- lsp
-    { name = "luasnip" }, -- snippets
-    { name = "buffer" }, -- text within current buffer
-    { name = "path" }, -- file system paths
-  }),
-  -- configure lspkind for vs-code like icons
-  formatting = {
-    format = lspkind.cmp_format({
-      maxwidth = 50,
-      ellipsis_char = "...",
-    }),
-  },
+	snippet = {
+		expand = function(args)
+			luasnip.lsp_expand(args.body)
+		end,
+	},
+	mapping = cmp.mapping.preset.insert({
+		["<C-k>"] = cmp.mapping.select_prev_item(), -- previous suggestion
+		["<C-j>"] = cmp.mapping.select_next_item(), -- next suggestion
+		["<C-b>"] = cmp.mapping.scroll_docs(-4),
+		["<C-f>"] = cmp.mapping.scroll_docs(4),
+		["<C-Space>"] = cmp.mapping.complete(), -- show completion suggestions
+		["<C-e>"] = cmp.mapping.abort(), -- close completion window
+		["<CR>"] = cmp.mapping.confirm({ select = false }),
+	}),
+	-- sources for autocompletion
+	sources = cmp.config.sources({
+		{ name = "nvim_lsp" }, -- lsp
+		{ name = "luasnip" }, -- snippets
+		{ name = "buffer" }, -- text within current buffer
+		{ name = "path" }, -- file system paths
+	}),
+
+	-- configure lspkind for vs-code like icons
+	formatting = {
+		fields = { "kind", "abbr", "menu" },
+		format = lspkind.cmp_format({
+			mode = "symbol_text", -- options: 'text', 'text_symbol', 'symbol_text', 'symbol'
+			maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+			ellipsis_char = "...",
+			menu = { -- showing type in menu
+				nvim_lsp = "[LSP]",
+				path = "[Path]",
+				buffer = "[Buffer]",
+				luasnip = "[LuaSnip]",
+			},
+			-- Get more info about how to display colors for tailwindcss by visiting below link
+			-- https://www.reddit.com/r/neovim/comments/103zetf/how_can_i_get_a_vscodelike_tailwind_css/
+			-- TODO: Might potentially able to add some function for other UI library
+			before = function(entry, vim_item) -- for tailwind css autocomplete
+				if vim_item.kind == "Color" and entry.completion_item.documentation then
+					local _, _, r, g, b = string.find(entry.completion_item.documentation, "^rgb%((%d+), (%d+), (%d+)")
+					if r then
+						local color = string.format("%02x", r) .. string.format("%02x", g) .. string.format("%02x", b)
+						local group = "Tw_" .. color
+						if vim.fn.hlID(group) < 1 then
+							vim.api.nvim_set_hl(0, group, { fg = "#" .. color })
+						end
+						vim_item.kind = "●" -- or "■"  or anything
+						vim_item.kind_hl_group = group
+						return vim_item
+					end
+				end
+				vim_item.kind = lspkind.symbolic(vim_item.kind) and lspkind.symbolic(vim_item.kind) or vim_item.kind
+				return vim_item
+			end,
+		}),
+	},
 })

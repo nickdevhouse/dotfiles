d5ea44db320375b9de9a38f24d29588566ea1f9d
Nick <nickdevhouse@gmail.com>
2023-08-05 22:58:32 +1200

    Add Black as python formatter

--- .config/nvim/lua/nickyang/plugins/lsp/mason.lua
+++ .config/nvim/lua/nickyang/plugins/lsp/mason.lua
@@ -41,6 +41,7 @@ mason_null_ls.setup({
 		"prettier", -- ts/js formatter
 		"stylua", -- lua formatter
 		"eslint_d", -- ts/js linter
+		"black", -- python formatter
 	},
 	-- auto-install configured formatters & linters (with null-ls)
 	automatic_installation = true,
